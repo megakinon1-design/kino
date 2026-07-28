4b0084777a4d703cf829114d055ec47ffa72439f
megakinon1-design <mega.kino.n1@gmail.com>
2026-07-28 17:35:27 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,5 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = os.getenv("ADMIN_ID")
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN topilmadi")
-if not ADMIN_ID:
-    raise ValueError("ADMIN_ID topilmadi")
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL topilmadi")
-
-ADMIN_ID = int(ADMIN_ID)
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8886984287:AAHswhlxzX4munSoApXbihlGBE0QV3b8-H0")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "6214685781"))
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://zafar_user:ekc9zVC6UOBh2STfPOkFIpDQQmYcRbK7@dpg-d9bmk2jtqb8s73cvq9d0-a.virginia-postgres.render.com/zafar")
